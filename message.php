76c86c978ad836b82f79badc2d5d8d266156afbb
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-07-11 18:53:49 +0530

    small changes

--- message.php
+++ message.php
@@ -2,14 +2,15 @@
   $name = htmlspecialchars($_POST['name']);
   $phone = htmlspecialchars($_POST['phone']);
   $email = htmlspecialchars($_POST['email']);
+  $message = htmlspecialchars($_POST['message']);
 //   $website = htmlspecialchars($_POST['address']);
- $message = htmlspecialchars($_POST['address']);
+
 
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
       $receiver = "maildemoacc@gmail.com"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
-      $body = "Name: $name\nPhone: $phone\nEmail: $email\nMessage: $website\n \n\nRegard,\n$name";
+      $body = "Name: $name\nPhone: $phone\nEmail: $email\nMessage: $message \n\nRegard,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
         header("Location: thankyou.html");
